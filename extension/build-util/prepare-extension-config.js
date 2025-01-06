2a89ba227dc1ecb04ec5d55de088e28d9a654048
Jette Petzold <jep@informatik.uni-kiel.de>
2025-01-06 16:40:35 +0100

    fixing names for renaming

--- extension/build-util/prepare-extension-config.js
+++ extension/build-util/prepare-extension-config.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 console.log("HI");
 
 var data = fs.readFileSync('filelist.txt', 'utf-8');
-var newValue = data.replace(/^\./gim, 'myString');
+var newValue = data.replace('"main": "./pack/src/extension.cjs"', '"main": "./pack/src/extension.js"');
 fs.writeFileSync('filelistSync.txt', newValue, 'utf-8');
 
 console.log('Bad magic');
