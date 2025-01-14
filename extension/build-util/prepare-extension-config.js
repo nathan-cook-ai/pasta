c28da3980c2adaf27a3eff9efa03b6e585203987
Jette Petzold <jep@informatik.uni-kiel.de>
2025-01-14 15:03:45 +0100

    updated node and vsce version to fix "yarn package"

--- extension/build-util/prepare-extension-config.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'node:fs';
-
-var data = fs.readFileSync('package.json', 'utf-8');
-var newValue = data.replace('"main": "./pack/src/extension.cjs"', '"main": "./pack/src/extension.js"');
-fs.writeFileSync('package.json', newValue, 'utf-8');
-
-console.log('Bad magic');
